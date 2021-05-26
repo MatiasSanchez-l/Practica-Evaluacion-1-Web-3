81604e2697447c10e8725e8c2d4f5f099afa319b
sanchez matias <matias-leandro@outlook.com>
2021-05-26 12:56:58 -0300

    commit 2

--- PracticaParcial1/Controllers/MamiferoController.cs
+++ PracticaParcial1/Controllers/MamiferoController.cs
@@ -11,6 +11,7 @@ namespace PracticaParcial1.Controllers
 {
     public class MamiferoController : Controller
     {
+        
         public static List<Mamifero> mamiferos= new List<Mamifero>();
         public IActionResult Index()
         {
@@ -61,9 +62,23 @@ namespace PracticaParcial1.Controllers
         }
 
         [HttpPost]
-        public IActionResult crearArdilla(Ardilla humano)
+        public IActionResult crearArdilla(Ardilla ardilla)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return View(ardilla);
+            }
+
+            if (mamiferos.Count == 0)
+            {
+                ardilla.Id = 1;
+            }
+            else
+            {
+                ardilla.Id = mamiferos.Max(o => o.Id) + 1;
+            }
+            mamiferos.Add(ardilla);
+            return RedirectToAction("listaMamiferos");
         }
 
         public IActionResult crearMono()
@@ -72,14 +87,64 @@ namespace PracticaParcial1.Controllers
         }
 
         [HttpPost]
-        public IActionResult crearMono(Mono humano)
+        public IActionResult crearMono(Mono mono)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return View(mono);
+            }
+
+            if (mamiferos.Count == 0)
+            {
+                mono.Id = 1;
+            }
+            else
+            {
+                mono.Id = mamiferos.Max(o => o.Id) + 1;
+            }
+            mamiferos.Add(mono);
+            return RedirectToAction("listaMamiferos");
         }
 
         public IActionResult listaMamiferos()
         {
+
+            return View(mamiferos);
+        }
+
+        public IActionResult VerDetalle(int Id)
+        {
+            if (Id == null)
+            {
+                return RedirectToAction("listaMamiferos");
+            }
+            ViewData["mamifero"] = mamiferos.FirstOrDefault(o => o.Id == Id);
             return View();
+        }
+
+        public IActionResult HacerAccion(int Id, string Accion)
+        {
+            if (Id == null || Accion == null)
+            {
+                return RedirectToAction("listaMamiferos");
+            }
+            Mamifero mamifero = mamiferos.FirstOrDefault(o => o.Id == Id);
+            if (Accion == "respirar")
+            {
+                mamifero.respirar();
+            }
+
+            if (Accion == "caminar")
+            {
+                mamifero.caminar();
+            }
+
+            if (Accion == "pensar")
+            {
+                mamifero.pensar();
+            }
+            ViewData["mamifero"] = mamifero;
+            return View("VerDetalle");
         }
 
     }
---
+++ PracticaParcial1/Services/IMamiferoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaParcial1.Services
+{
+    public interface IMamiferoService
+    {
+
+    }
+}
